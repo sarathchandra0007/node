d41bf5b33f85eb4f6f34b3f92eccb356f660919c
Sarath <sarath@esthenos.com>
2019-02-05 22:01:05 +0530

    Added routes for middlewares

--- express_project/app.js
+++ express_project/app.js
@@ -4,18 +4,20 @@ const express = require('express');
 
 const app = express();
 
-// use alows us to add new middleware function
+// "use" alows us to add new middleware function
 // (() => {}) --> creating function
-app.use((req, res, next) => {
+app.use('/add-url', (req, res, next) => {
     console.log('I am in the middleware');
-    next() // Allows request to go to next middleware
+    res.send('<h1> In "add-url" route</h1>');
+    // next()  Allows request to go to next middleware
 });    
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('I am in another middleware')
     res.send('<h1> Exited to learn express</h1>');
 });   
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
+// const server = http.createServer(app);
+ 
+// server.listen(3000);
