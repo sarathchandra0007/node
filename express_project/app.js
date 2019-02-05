4c6421dc8b0145a42a972bfd88b29bf827a75b8a
Sarath <sarath@esthenos.com>
2019-02-05 22:47:14 +0530

    Parsing data entered in form

--- express_project/app.js
+++ express_project/app.js
@@ -4,13 +4,23 @@ const express = require('express');
 
 const app = express();
 
+const bodyParser = require('body-parser')
+
+// For text parsing
+app.use(bodyParser.urlencoded({extended: false}));
+
 // "use" alows us to add new middleware function
 // (() => {}) --> creating function
-app.use('/add-url', (req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('I am in the middleware');
-    res.send('<h1> In "add-url" route</h1>');
+    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><button>Add</button></form>');
     // next()  Allows request to go to next middleware
-});    
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/')
+});
 
 app.use('/', (req, res, next) => {
     console.log('I am in another middleware')
