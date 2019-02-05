60cc1f0093e4b74d83e85e6635832dcd332c8086
Sarath <sarath@esthenos.com>
2019-02-05 23:57:12 +0530

    Split project into different modules

--- express_project/app.js
+++ express_project/app.js
@@ -1,33 +1,17 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
+
 const bodyParser = require('body-parser')
 
-// For text parsing
 app.use(bodyParser.urlencoded({extended: false}));
 
-// "use" alows us to add new middleware function
-// (() => {}) --> creating function
-app.use('/add-product', (req, res, next) => {
-    console.log('I am in the middleware');
-    res.send('<form action = "/product" method = "POST"><input type = "text" name = "title"><button>Add</button></form>');
-    // next()  Allows request to go to next middleware
-});
+app.use(adminRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-});
-
-app.use('/', (req, res, next) => {
-    console.log('I am in another middleware')
-    res.send('<h1> Exited to learn express</h1>');
-});   
+app.use(shopRoutes);
 
 app.listen(3000);
-// const server = http.createServer(app);
- 
-// server.listen(3000);
